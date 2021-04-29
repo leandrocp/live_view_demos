d3a16d69c93f44c9df680d0cdc2e2a32c7318326
Leandro Cesquini Pereira <leandro@leandro.io>
2021-04-29 16:13:12 -0400

    Reload tweets after updating DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,15 @@ import topbar from "topbar"
 import {LiveSocket} from "phoenix_live_view"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+
+let Hooks = {}
+Hooks.ReloadTweets = {
+  updated() {
+    twttr.widgets.load()
+  }
+}
+
+let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: Hooks})
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
