4f79ab671541900d312cd8639229cc9113b6cee3
Leandro Cesquini Pereira <leandro@leandro.io>
2022-01-11 11:07:35 -0500

    Make sure script is loaded

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,55 +1,31 @@
-// If you want to use Phoenix channels, run `mix help phx.gen.channel`
-// to get started and then uncomment the line below.
-// import "./user_socket.js"
-
-// You can include dependencies in two ways.
-//
-// The simplest option is to put them in assets/vendor and
-// import them using relative paths:
-//
-//     import "./vendor/some-package.js"
-//
-// Alternatively, you can `npm install some-package` and import
-// them using a path starting with the package name:
-//
-//     import "some-package"
-//
-
-// Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
-// Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
-window.twttr = (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0], t = window.twttr || {}
-  if (d.getElementById(id)) return
-  js = d.createElement(s)
-  js.id = id
-  js.src = "https://platform.twitter.com/widgets.js"
-  fjs.parentNode.insertBefore(js, fjs)
-
-  t._e = []
-  t.ready = function(f) {
-    t._e.push(f)
-  };
-
-  return t
-}(document, "script", "twitter-wjs"))
-
-let loadTweets = function() {
+const renderTweets = function() {
   const elements = document.getElementsByClassName("tweet")
 
   for (let i=0; i < elements.length; i++) {
     let el = elements[i]
+    let id = el.dataset.tweetid
+    window.twttr.widgets.createTweetEmbed(id, el, {dnt: true, conversation: "none"})
+  }
+}
 
-    window.twttr.widgets.createTweet(el.dataset.tweetid, el, {})
-    .then((r) => {
-      const id = el.dataset.tweetid
-      let facadeEl = document.getElementById("facade-" + id)
-      facadeEl.parentNode.removeChild(facadeEl);
-    })
+const injectScript = function(callback) {
+  let script = document.createElement("script")
+  script.setAttribute("src", "//platform.twitter.com/widgets.js")
+  script.setAttribute("async", true)
+  script.onload = () => callback()
+  document.body.appendChild(script)
+}
+
+const load = function() {
+  if (!window.twttr) {
+    injectScript(() => renderTweets())
+  } else {
+    renderTweets()
   }
 }
 
@@ -57,38 +33,20 @@ let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("
 
 let Hooks = {}
 
-Hooks.CreateTweet = {
-  mounted() {
-    window.twttr.widgets.createTweet(this.el.dataset.tweetid, this.el, {})
-    .then((el) => {
-      const id = this.el.dataset.tweetid
-      let facadeEl = document.getElementById("facade-" + id)
-      facadeEl.parentNode.removeChild(facadeEl);
-    })
-  }
-}
-
 Hooks.RenderTweets = {
   mounted() {
-    loadTweets()
+    load()
   },
   updated() {
-    loadTweets()
+    load()
   }
 }
 
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: Hooks})
 
-// Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#5B21B6"}, shadowColor: "rgba(0, 0, 0, .3)"})
 window.addEventListener("phx:page-loading-start", info => topbar.show())
 window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 
-// connect if there are any LiveViews on the page
 liveSocket.connect()
-
-// expose liveSocket on window for web console debug logs and latency simulation:
-// >> liveSocket.enableDebug()
-// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
-// >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
