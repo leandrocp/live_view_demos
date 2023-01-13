71faf916b41737550fe61be2a23b4b9122fa48b3
Leandro Cesquini Pereira <leandro@leandro.io>
2023-01-13 11:53:37 -0500

    replace node tailwind with hex tailwind

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,12 +1,23 @@
+const plugin = require("tailwindcss/plugin")
+
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './js/**/*.js',
-    '../lib/*_web/**/*.*ex'
+  content: [
+    "./js/**/*.js",
+    "../lib/*_web.ex",
+    "../lib/*_web/**/*.*ex"
   ],
-  theme: {},
-  variants: {},
+  theme: {
+    extend: {
+      colors: {
+        brand: "#FD4F00",
+      }
+    },
+  },
   plugins: [
-    require('tailwindcss-multi-column')()
+    require("@tailwindcss/forms"),
+    plugin(({addVariant}) => addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])),
+    plugin(({addVariant}) => addVariant("phx-click-loading", [".phx-click-loading&", ".phx-click-loading &"])),
+    plugin(({addVariant}) => addVariant("phx-submit-loading", [".phx-submit-loading&", ".phx-submit-loading &"])),
+    plugin(({addVariant}) => addVariant("phx-change-loading", [".phx-change-loading&", ".phx-change-loading &"]))
   ]
 }
--- mix.exs
+++ mix.exs
@@ -17,9 +17,6 @@ defmodule LiveViewCollection.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {LiveViewCollection.Application, []},
@@ -27,13 +24,9 @@ defmodule LiveViewCollection.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
@@ -49,26 +42,18 @@ defmodule LiveViewCollection.MixProject do
       {:phoenix_live_reload, "~> 1.4", only: :dev},
       {:phoenix_live_view, "0.17.5"},
       {:plug_cowboy, "~> 2.6"},
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:yaml_elixir, "~> 2.9"}
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get"],
-      "assets.deploy": [
-        "cmd --cd assets npm run deploy",
-        "esbuild default --minify",
-        "phx.digest"
-      ]
+      setup: ["deps.get", "assets.setup"],
+      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
     ]
   end
 end
